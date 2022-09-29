f4b93a38958902c1b8b7141cbd71da179b5fe55c
alex <alex@pdp7.net>
2022-09-29 20:19:46 +0200

    Add blocks parsing and don't parse empty lists

--- blocks.pl
+++ blocks.pl
@@ -1,13 +1,18 @@
-:- module(blocks, [unordered_list/3]).
+:- module(blocks, [blocks/3, block/3, unordered_list/3]).
 
 :- set_prolog_flag(double_quotes, chars).
 
 :- use_module(inlines).
 
+blocks([X|XS]) --> block(X), blocks(XS).
+blocks([]) --> [].
+
+block(uol(X)) --> unordered_list(X), !.
+
 % Unordered list
 
 unordered_list([X|XS]) --> unordered_list_item(X), unordered_list(XS).
-unordered_list([]) --> [].
+unordered_list([X]) --> unordered_list_item(X).
 unordered_list_item(uoli("*", " ", PT, "\n")) -->
     "* ",
     [X|XS],
