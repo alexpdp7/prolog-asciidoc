d5fd3f3715914f2faa1b3422874932fccf0288b3
alex <alex@pdp7.net>
2022-11-12 12:03:24 +0100

    Fix #6

--- Containerfile
+++ Containerfile
@@ -1,4 +1,6 @@
 FROM quay.io/centos/centos:stream9
+ARG QUAY_EXPIRES_AFTER
+LABEL quay.expires-after=$QUAY_EXPIRES_AFTER
 RUN dnf install -y epel-release
 RUN dnf install -y pl python3-pip
 RUN pip install poetry
--- build.py
+++ build.py
@@ -17,20 +17,11 @@ def is_dirty():
     return returncode == 1
 
 
-def build_image():
+def build_images_and_push():
     tags = ["dirty"] if is_dirty() else [describe(False), describe(True)]
     for tag in tags:
         quay_expires_after = "never" if not is_dirty() and tag == "main" else "3d"
-        subprocess.run(["podman", "build", ".", "--label", f"quay.expires-after={quay_expires_after}", "-t", f"prolog-asciidoc:{tag}"], check=True)
-    return tags
-
-
-def build_image_and_push():
-    tags = build_image()
-    for tag in tags:
+        subprocess.run(["podman", "build", ".", "--build-arg", f"QUAY_EXPIRES_AFTER={quay_expires_after}", "-t", f"prolog-asciidoc:{tag}"], check=True)
         assert tag != "dirty", "attempting to push dirty image"
         subprocess.run(["podman", "push", f"prolog-asciidoc:{tag}", f"quay.io/alexpdp7/prolog-asciidoc:{tag}"], check=True)
-
-
-if __name__ == "__main__":
-    build_image_and_push()
+    return tags
